58b86fc5f4caa0ec5e9f29ff501d03bcbc4c4a0b
Your Name <you@example.com>
2021-05-17 18:35:24 +0300

    5

---
+++ multirotors.js
@@ -0,0 +1,112 @@
+﻿ var multirotors = [
+        {
+            "name": "Draganfly Innovations Inc. (Канада)\r\nDraganflyer X6",
+            "m": "0.5",
+            "tmax": "15",
+            "h": "500",
+            "r": "1",
+            "tr": "3"
+        },
+        {
+            "name": "Dji AGRAS MG-1P (Китай)",
+            "m": "24.8",
+            "tmax": "20",
+            "h": "2000",
+            "r": "5"
+        },
+        {
+            "name": "Aeryon Labs Inc. (Канада)\r\nScout",
+            "m": "0.4",
+            "tmax": "25",
+            "h": "500",
+            "r": "3",
+            "tr": "3"
+        },
+        {
+            "name": "Cyber Technology (Австралия)\r\nCyberQuad Maxi",
+            "m": "0.5",
+            "tmax": "25",
+            "h": "500",
+            "r": "1",
+            "tr": "3"
+        },
+        {
+            "name": "Dji PHANTOM 3 SE (Китай)",
+            "tmax": "25",
+            "h": "6000",
+            "r": "4"
+        },
+        {
+            "name": "Dji MAVIC PRO (Китай)",
+            "tmax": "27",
+            "h": "5000",
+            "r": "7"
+        },
+        {
+            "name": "ZALA 421-21",
+            "m": "1.5",
+            "tmax": "30",
+            "h": "2500",
+            "r": "2",
+            "tr": "3"
+        },
+        {
+            "name": "Dji PHANTOM 4 ADVANCED \r\n(Китай)",
+            "tmax": "30",
+            "h": "6000",
+            "r": "7"
+        },
+        {
+            "name": "Dji MAVIC PRO PLATINUM\r\n (Китай)",
+            "tmax": "30",
+            "h": "5000",
+            "r": "7"
+        },
+        {
+            "name": "Dji MATRICE М 210 (Китай)",
+            "m": "6",
+            "tmax": "30",
+            "h": "3000",
+            "r": "8"
+        },
+        {
+            "name": "3D Robotics (США)\r\nArduCopter Hexa",
+            "m": "1",
+            "tmax": "30 (0,5 кг)",
+            "h": "500",
+            "r": "1",
+            "tr": "5"
+        },
+        {
+            "name": "ЗАО «Научно-производственный\r\n центр Фирма «НЕЛК»\r\n\"Колибри\" ",
+            "m": "2",
+            "tmax": "30 (2кг) - 60 (1 кг)",
+            "h": "500",
+            "r": "10",
+            "tr": "5"
+        },
+        {
+            "name": "Dji MATRICE 600PRO (Китай)",
+            "m": "15.5",
+            "tmax": "32",
+            "h": "4500",
+            "r": "5"
+        },
+        {
+            "name": "AirRobot Australasia Pty Ltd (Австралия) AR 150",
+            "m": "1",
+            "tmax": "40",
+            "h": "500",
+            "r": "1",
+            "tr": "3"
+        },
+        {
+            "name": "Microdrones Gmbh (Германия)\r\nmd4-1000\r\n",
+            "m": "1.2",
+            "tmax": "60 (0,5 кг)",
+            "h": "500",
+            "r": "2",
+            "tr": "10"
+        }
+
+    ];
