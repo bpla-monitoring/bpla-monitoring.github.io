a57f06e7afb5abf2bed5b55d8d48599fd00d4ffa
Your Name <you@example.com>
2021-06-13 12:17:13 +0300

    1

--- sw.js
+++ sw.js
@@ -2,10 +2,14 @@
 
 importScripts('sw-toolbox.js');
 
-toolbox.precache(["index.html","style.css"]);
+toolbox.precache(["index.html","index.css",
+"prognoz.html"
+
+
+]);
 
 toolbox.router.get('/images/*', toolbox.cacheFirst);
-
+toolbox.router.get('/icons/*', toolbox.cacheFirst);
 toolbox.router.get('/*', toolbox.networkFirst, {
   networkTimeoutSeconds: 5
 });
